18bcf1e9eb67b1ea394335f9bb12fbadf75bae75
Yuvaraj Govindarajan <47356679+gyuv@users.noreply.github.com>
2026-08-13 01:31:55 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,20 @@
 import React from "react";
+import CyberBackground from "@/components/CyberBackground";
 import SidebarNav from "@/components/SidebarNav";
 import HeroGaming from "@/components/HeroGaming";
-import AboutSectionGaming from "@/components/AboutSectionGaming";
-import ExperienceGaming from "@/components/ExperienceGaming";
-import ProjectsGaming from "@/components/ProjectsGaming";
-import SkillsGaming from "@/components/SkillsGaming";
-import ContactGaming from "@/components/ContactGaming";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[#020617] text-white">
+    <div className="min-h-screen bg-[#020617] text-white relative selection:bg-cyan-500 selection:text-black font-sans">
+      {/* 3D WebGL Particle Starfield Background */}
+      <CyberBackground />
+
+      {/* Fixed Left Sidebar Navigation */}
       <SidebarNav />
-      <main className="lg:pl-72 flex flex-col min-h-screen">
+
+      {/* Main Container Offset for Sidebar */}
+      <main className="lg:pl-80 flex flex-col min-h-screen relative z-10">
         <HeroGaming />
-        <AboutSectionGaming />
-        <ExperienceGaming />
-        <ProjectsGaming />
-        <SkillsGaming />
-        <ContactGaming />
       </main>
     </div>
   );
